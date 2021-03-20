6fb23c48b5626f012a736f0a90a343712fb9de0a
andreacolina <a.colina.2020@alumnos.urjc.es>
2021-03-21 00:43:39 +0100

    exec.js

--- exec.js
+++ exec.js
@@ -1,39 +1,31 @@
-const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 const dockerHubUser = 'andreajuanmaurjc';
- function build(serviceName, command){
- 
-   console.log(`Stated service [${serviceName}]`);
- 
-   let cmd = spawn(command, [], { cwd: serviceName, shell: true });
-   
-   cmd.stdout.on('data', function(data){
-     process.stdout.write(`[${serviceName}] ${data}`);
-   });
- 
-   cmd.stderr.on('data', function(data){
-     process.stderr.write(`[${serviceName}] ${data}`);
-   });
- }
- 
- function upload(image){
-   let command = `docker push ${image}`
-  let cmd = spawn(command, [], { shell: true });
-  cmd.stdout.on('data', function(data){
-    process.stdout.write(`[${serviceName}] ${data}`);
+function build(serviceName, command) {
+  console.log(`Building docker image for [${serviceName}]`);
+  console.log(`Command: ${command}`);
+
+  spawnSync(command, [], {
+    shell: true,
+    stdio: 'inherit'
   });
+}
 
-  cmd.stderr.on('data', function(data){
-    process.stderr.write(`[${serviceName}] ${data}`);
+function upload(image) {
+  let command = `docker push ${image}`
+  console.log(`Command: ${command}`);
+  spawnSync(command, [], {
+    shell: true,
+    stdio: 'inherit'
   });
- }
+}
 
- 
- build('weatherservice', `pack build ${dockerHubUser}/weatherservice:v1 --path . --builder gcr.io/buildpacks/builder:v1`);
- build('toposervice', `mvn compile jib:build -Dimage=${dockerHubUser}/toposervice`);
- build('server',`docker build -t ${dockerHubUser}/server .`);
- build('planner',`docker build -t ${dockerHubUser}/planner .`);
 
- upload(`${dockerHubUser}/weatherservice:v1`);
- upload(`${dockerHubUser}/toposervice`);
- upload(`${dockerHubUser}/server`);
- upload(`${dockerHubUser}/planner`);
+build('weatherservice', `pack build ${dockerHubUser}/weatherservice:v1 --path . --builder gcr.io/buildpacks/builder:v1`);
+build('toposervice', `mvn compile jib:build -Dimage=${dockerHubUser}/toposervice`);
+build('server', `docker build -t ${dockerHubUser}/server .`);
+build('planner', `docker build -t ${dockerHubUser}/planner .`);
+
+upload(`${dockerHubUser}/weatherservice:v1`);
+upload(`${dockerHubUser}/toposervice`);
+upload(`${dockerHubUser}/server`);
+upload(`${dockerHubUser}/planner`);
